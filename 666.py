d03d942bd8bfe9e39afca95d0f6e8d6b74989094
manasaa22 <121430751+manasaa22@users.noreply.github.com>
2024-01-12 10:51:16 +0530

    Update 666.py

--- 666.py
+++ 666.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('HELLO')
-st.write('HELLO')
+st.write('meow meow')
 st.image('https://www.shutterstock.com/image-vector/cute-ginger-cat-waving-hand-260nw-1038445837.jpg ',caption='puppy')
