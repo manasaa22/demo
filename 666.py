4376911b4b666f500c531d251e96efeceb11b49f
manasaa22 <121430751+manasaa22@users.noreply.github.com>
2024-01-12 10:30:04 +0530

    Create 666.py

---
+++ 666.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.header('puppy')
+st.write('puppy')
+st.image('https://images.nightcafe.studio/jobs/XBKobupTtwNaHoGHDpEv/XBKobupTtwNaHoGHDpEv--1--q50wa.jpg',caption='puppy')
